4d60894dcd56c848adf4dbd1cbd23e23a9493ed7
sangya-25 <ojha.sangya25@gmail.com>
2025-06-20 13:00:37 +0530

    server.js updated

--- eduConnect/server.js
+++ eduConnect/server.js
@@ -103,7 +103,9 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+module.exports = app;
